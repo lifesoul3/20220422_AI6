8ba1152a0c90d21fa3969e32f3140d64d88d9683
lifesoul3 <lifesoul3@naver.com>
2022-06-24 15:10:33 +0900

    .

---
+++ 6m24d/6m24d/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6m24d
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Locale> locales = KakaoApi.Search(textBox1.Text);
+            listBox1.Items.Clear();
+
+            foreach(Locale item in locales)
+            {
+                listBox1.Items.Add(item);
+            }
+
+        }
+        private void button1_KeyUp(object sender, KeyEventArgs e)
+        {
+        }
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+                button1.PerformClick();//button1 클릭하는 거임
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            Locale ml = listBox1.SelectedItem as Locale;
+            object[] pos = new object[] { ml.Lat, ml.Lng };
+            HtmlDocument hdoc = webBrowser1.Document;
+            hdoc.InvokeScript("setCenter", pos);
+        }
+    }
+}
---
+++ 6m24d/6m24d/KakaoApi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace _6m24d
+{
+    public class KakaoApi
+    {
+        public static List<Locale>  Search (string text)
+        {
+            List<Locale> list = new List<Locale>();
+            string site = "https://dapi.kakao.com/v2/local/search/keyword.json";
+            string query = $"{site}?query={text}";
+            string restApikey = "3e9f88bb1bd92b4f36599ffae4542247";
+            string header = $"KakaoAK {restApikey}";
+            
+            WebRequest request =WebRequest.Create(query);
+            request.Headers.Add("Authorization", header);
+
+            //응답받기
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream,Encoding.UTF8);
+
+            string json = reader.ReadToEnd();
+                
+            JavaScriptSerializer js=new JavaScriptSerializer();
+
+            
+            dynamic dob=js.Deserialize<dynamic>(json);
+            dynamic docs = dob["documents"];
+            object[] buf = docs;
+
+            int length = buf.Length;
+            for(int i = 0; i < length; i++)
+            {
+                string lname = docs[i]["place_name"];
+                double x = double.Parse(docs[i]["x"]);
+                double y = double.Parse(docs[i]["y"]);
+                list.Add(new Locale(lname, y, x));
+
+            }
+
+
+
+            return list;
+
+        //Authorization: KakaoAK { REST_API_KEY};
+        }
+
+
+    }
+}
